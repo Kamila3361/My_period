95832fa4f1e7a82e1d7be5179d73fed05328882b
Kamila <tashimovakamila1@gmail.com>
2023-09-29 23:36:55 +0600

    set secret key

--- a_period_tracker/settings.py
+++ a_period_tracker/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-by&e9qsnv$352gx_)th_2zjg8cbj^9erwx+-v+(7+3^rw2#n7d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
