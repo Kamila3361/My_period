ff46ecd962822063715a47abd1aeda8a6224a433
Kamila <tashimovakamila1@gmail.com>
2023-09-30 00:14:57 +0600

    view

--- my_period/views.py
+++ my_period/views.py
@@ -4,7 +4,7 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
 from .models import PeriodCycle, PersonalInfo, ChatInfo
-from ..secret_key import API_KEY
+from .secret_key import API_KEY
 
 from rest_framework.response import Response
 from rest_framework import status, generics
